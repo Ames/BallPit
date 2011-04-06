b571f05f9a197f8e688051ae7fbc0d4d19ff359e
Ames Bielenberg <amesbielenberg@gmail.com>
2011-04-06 14:53:15 -0400

    whitespace

--- favGen.js
+++ favGen.js
@@ -1,28 +1,28 @@
 var favLink;
 
 var setFavCol=function(col,notify){
-    
-    var canvas = document.createElement('canvas'),
-        ctx;
-    if (canvas.getContext) {
-      canvas.height = canvas.width = 16; // set the size
-      ctx = canvas.getContext('2d');
-      if(notify){
-          ctx.fillStyle = "black";
-          ctx.beginPath();
-          ctx.arc(8,8,8,0,2*Math.PI);
-          ctx.closePath();
-      }
-      ctx.fillStyle = col;
-      ctx.beginPath();
-      ctx.arc(8,8,7,0,2*Math.PI);
-      ctx.closePath();
-      ctx.fill();
-
-
+	
+	var canvas = document.createElement('canvas'), ctx;
+	
+	if (canvas.getContext) {
+		canvas.height = canvas.width = 16; // set the size
+		ctx = canvas.getContext('2d');
+		if(notify){
+			ctx.fillStyle = "black";
+			ctx.beginPath();
+			ctx.arc(8,8,8,0,2*Math.PI);
+			ctx.closePath();
+		}
+		ctx.fillStyle = col;
+		ctx.beginPath();
+		ctx.arc(8,8,6.5,0,2*Math.PI);
+		ctx.closePath();
+		ctx.fill();
+		
+		
 		var head=document.getElementsByTagName('head')[0];
 		//var head=document.getElementsByTagName('body')[0];
-
+		
 		if(!favLink){
 			favLink=document.createElement('link');
 			favLink.rel='icon';
@@ -31,7 +31,7 @@ var setFavCol=function(col,notify){
 		}
 		
 		favLink.href = canvas.toDataURL('image/png');
-    }
+	}
 }
 
 var removeFav=function(){
@@ -40,5 +40,3 @@ var removeFav=function(){
 	head.removeChild(favLink);
 	favLink=undefined;
 }
-
-
